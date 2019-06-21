1b70c5effd20c2b6495b0d03023fa8f2afbe3bcc
JohnKouGr <koukoulogiannis.yiannis@gmail.com>
2019-06-21 21:05:16 +0300

    Initial commit

---
+++ src/main/java/com/proj/defaulter/TrainersApplication.java
@@ -0,0 +1,13 @@
+package com.proj.defaulter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainersApplication.class, args);
+	}
+
+}
